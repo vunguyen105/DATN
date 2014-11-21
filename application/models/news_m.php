1dfac77d65fb45a7e52ce8bad3e83add19a5773f
Vu <vunguyen105@gmail.com>
2014-11-21 10:08:36 +0000

    Update news_m.php

--- application/models/news_m.php
+++ application/models/news_m.php
@@ -5,5 +5,18 @@
         public function __construct(){
             parent::__construct();
         }
+        
+        public $rules = array (
+			'name' => array (
+					'field' => 'NewTitle',
+					'label' => 'Tiêu đề',
+					'rules' => 'trim|required|xss_clean' 
+			),
+			'price' => array (
+					'field' => 'NewContent',
+					'label' => 'Nội dung',
+					'rules' => 'trim|required|xss_clean' 
+			)
+	);
     }
 ?>
