75b35400c6d15b9b7089df2ca9384c4c00047b1d
Vu <vunguyen105@gmail.com>
2014-11-27 07:54:22 +0000

    27
    27

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -45,8 +45,46 @@ class home extends CI_Controller {
         }
 
         public function product() {
+            $this->load->model('menucate_m');
+            $this->load->model('product_m');
+            $this->load->library('category_lib');
+            $this->new_nested_set = $this->category_lib->category_initialize();
+            $idMenu = 4;
+            $cateInMenu = $this->menucate_m->getCateMenu($idMenu);
+            $inCat = array();
+            foreach ($cateInMenu as $k => $v) {
+                    $inCat[] = $v['CateID'];
+            }
+            $this->product_m->set_start( 0, 3);
+            $data['products'] = $this->product_m->get();
+            //var_dump($data['products']);die;
+            $data['menu'] = $this->new_nested_set->category_fronend( 1, $inCat, $idMenu, $cateInMenu);
             $data['slider'] = $this->load->view('frontend/slider','', true);
-            $data['content'] = $this->load->view('frontend/content','', true);
+            $data['content'] = $this->load->view('frontend/content',$data, true);
+            $this->load->view('frontend/layout',$data);
+        }
+        
+        public function blog() {
+            $this->load->model('menucate_m');
+            $this->load->model('product_m');
+            $this->load->model('news_m');
+            $this->load->library('category_lib');
+            $this->new_nested_set = $this->category_lib->category_initialize();
+            $idMenu = 4;
+            $cateInMenu = $this->menucate_m->getCateMenu($idMenu);
+            $inCat = array();
+            foreach ($cateInMenu as $k => $v) {
+                    $inCat[] = $v['CateID'];
+            }
+            $data['menu'] = $this->new_nested_set->category_fronend( 1, $inCat, $idMenu, $cateInMenu);
+            $data['news'] = $this->news_m->get();
+            $data['content'] = $this->load->view('frontend/blog',$data, true);
+            $this->load->view('frontend/layout',$data);
+        }
+        
+        public function contact() {
+            $data ='';
+            $data['content'] = $this->load->view('frontend/contact_us',$data, true);
             $this->load->view('frontend/layout',$data);
         }
 }
