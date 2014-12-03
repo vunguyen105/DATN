853536b30ce83aeb06389774fba501a55d59c616
Vu <vunguyen105@gmail.com>
2014-12-03 17:22:36 +0700

    Update login.php

--- application/views/back/login.php
+++ application/views/back/login.php
@@ -37,7 +37,7 @@
 	<div class="content">
 		<!-- BEGIN LOGIN FORM -->
 		<form class="form-vertical login-form" action="<?php echo base_url()?>backend/login" method="post">
-			<h3 class="form-title">Login to your account</h3>
+			<h3 class="form-title">Đăng nhập vào hệ thống</h3>
 			<?php if(is_error_flashdata($this->session->flashdata('error'))) { ;?>
 			<div class="alert alert-error">
 				<span><?php echo $this->session->flashdata('error');?></span>
@@ -45,63 +45,40 @@
 			<?php }?>
 			<div class="control-group">
 				<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-				<label class="control-label visible-ie8 visible-ie9">Username</label>
+				<label class="control-label visible-ie8 visible-ie9">Tài khoản</label>
 				<div class="controls">
 					<div class="input-icon left">
 						<i class="icon-user"></i>
-						<input value="<?php echo set_value('username'); ?>" class="m-wrap placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
+						<input value="<?php echo set_value('username'); ?>" class="m-wrap placeholder-no-fix" type="text" autocomplete="off" placeholder="Tài khoản" name="username"/>
 					</div>
 				</div>
 			</div>
 			<div class="control-group">
-				<label class="control-label visible-ie8 visible-ie9">Password</label>
+				<label class="control-label visible-ie8 visible-ie9">Mật khẩu</label>
 				<div class="controls">
 					<div class="input-icon left">
 						<i class="icon-lock"></i>
-						<input value="<?php echo set_value('password'); ?>" class="m-wrap placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"/>
+						<input value="<?php echo set_value('password'); ?>" class="m-wrap placeholder-no-fix" type="password" autocomplete="off" placeholder="Mật khẩu" name="password"/>
 					</div>
 				</div>
 			</div>
 			<?php echo validation_errors('<div class="alert alert-error"><sup>','</sup></div>'); ?>
 			<div class="form-actions">
 				<label class="checkbox">
-				<input type="checkbox" name="remember" value="1"/> Remember me
+				<input type="checkbox" name="remember" value="1"/> Nhớ
 				</label>
 				<button type="submit" class="btn blue pull-right">
-				Login <i class="m-icon-swapright m-icon-white"></i>
+				Đăng nhập<i class="m-icon-swapright m-icon-white"></i>
 				</button>            
 			</div>
 			<div class="forget-password">
-				<h4>Forgot your password ?</h4>
+				<h4>Bạn quên mật khẩu ?</h4>
 				<p>
-					no worries, click <a href="javascript:;"  id="forget-password">here</a>
-					to reset your password.
+					nếu quên mật khẩu, click <a href="javascript:;"  id="forget-password">vào đây</a>
+					để lấy lại mật khẩu.
 				</p>
 			</div>
 		</form>
-		<!-- END LOGIN FORM -->        
-		<!-- BEGIN FORGOT PASSWORD FORM -->
-		<form class="form-vertical forget-form" action="index.html" method="post">
-			<h3 >Forget Password ?</h3>
-			<p>Enter your e-mail address below to reset your password.</p>
-			<div class="control-group">
-				<div class="controls">
-					<div class="input-icon left">
-						<i class="icon-envelope"></i>
-						<input class="m-wrap placeholder-no-fix" type="text" placeholder="Email" autocomplete="off" name="email" />
-					</div>
-				</div>
-			</div>
-			<div class="form-actions">
-				<button type="button" id="back-btn" class="btn">
-				<i class="m-icon-swapleft"></i> Back
-				</button>
-				<button type="submit" class="btn blue pull-right">
-				Submit <i class="m-icon-swapright m-icon-white"></i>
-				</button>            
-			</div>
-		</form>
-		<!-- END FORGOT PASSWORD FORM -->
 	</div>
 	<!-- END LOGIN -->
 </body>
